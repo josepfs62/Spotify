813220b6194685868d219dd95411a6e3bc133b92
Josep <jfayos@cifpfbmoll.eu>
2021-11-05 19:45:25 +0100

    Initial commit

---
+++ app/src/main/java/com/josepfayos/spotify/MainActivity.java
@@ -0,0 +1,21 @@
+package com.josepfayos.spotify;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.GradientDrawable;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.RoundRectShape;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
